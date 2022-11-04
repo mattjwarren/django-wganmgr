82871bcaf99adce09fa42165995b7258e7a3721a
matt <matthew_j_warren@hotmail.com>
2022-11-04 21:53:47 +0000

    dummy views for basic tables

--- wganmgr/wganbrowser/urls.py
+++ wganmgr/wganbrowser/urls.py
@@ -3,5 +3,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #/wganbrowser
     path('', views.index, name='index'),
+    #/wganbrowser/model/{model_id}/
+    path('model/<int:model_id>/',views.model_detail,name='model_detail'),
+    #/wganbrowser/model/{model_id}/
+    path('dataset/<int:dataset_id>/',views.dataset_detail,name='dataset_detail'),
+    #/wganbrowser/model/{model_id}/
+    path('modelrun/<int:modelrun_id>/',views.modelrun_detail,name='modelrun_detail'),
+    #/wganbrowser/model/{model_id}/
+    path('modelsnapshot/<int:modelsnapshot_id>/',views.modelsnapshot_detail,name='modelsnapshot_detail'),
+    #/wganbrowser/model/{model_id}/
+    path('library/<int:library_id>/',views.library_detail,name='library_detail'),
 ]
--- wganmgr/wganbrowser/views.py
+++ wganmgr/wganbrowser/views.py
@@ -6,3 +6,19 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the wgan browser index.")
+
+def model_detail(request,model_id):
+    return HttpResponse("Looking at details for model %s" % model_id)
+
+def dataset_detail(request,dataset_id):
+    return HttpResponse("Looking at details for dataset %s" % dataset_id)
+
+def modelrun_detail(request,modelrun_id):
+    return HttpResponse("Looking at details for modelrun %s" % modelrun_id)
+
+def modelsnapshot_detail(request,modelsnapshot_id):
+    return HttpResponse("Looking at details for modelsnapshot %s" % modelsnapshot_id)
+
+def library_detail(request,library_id):
+    return HttpResponse("Looking at details for library %s" % library_id)
+
