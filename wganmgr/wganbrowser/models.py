ee87fb7261f8bd8bf00fe1cb0c6e43bc0599987e
matt <matthew_j_warren@hotmail.com>
2022-11-04 00:22:09 +0000

    start models population

--- wganmgr/wganbrowser/models.py
+++ wganmgr/wganbrowser/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class modelSnapshot(models.Model):
+    modelRun = models.ForeignKey(modelRun)
+    checkpoint = models.IntegerField(default=0)
+    path = models.CharField(max_length=512) 
+
+class modelRun(models.Model):
+    model = models.ForeignKey(model)
+    path = models.CharField(max_length=512)
+    name = models.CharField(max_length=512)
+
+class model(models.Model):
+    name = models.CharField(max_length=512)
+    dataset = models.ForeignKey(dataset)
+
+
+    #https://docs.djangoproject.com/en/4.1/intro/tutorial02/
