1b3f750e71251e1f520d4506572cc831808d8784
matt <matthew_j_warren@hotmail.com>
2022-11-06 12:59:16 +0000

    more models pages and template inheritance

--- wganmgr/wganbrowser/urls.py
+++ wganmgr/wganbrowser/urls.py
@@ -5,18 +5,22 @@ from . import views
 app_name = 'wganbrowser'
 urlpatterns = [
     path('', views.index, name='index'),
+    #/wganbrowser/models/
+    path('models/',views.models,name='models'),
     #/wganbrowser/model_detail/{model_id}/
     path('model_detail/<int:model_id>/',views.model_detail,name='model_detail'),
     #/wganbrwoser/model_create/
     path('model_create/',views.model_create,name='model_create'),
     #/wganbrwoser/model_save/
     path('model_save/',views.model_save,name='model_save'),
-    #/wganbrowser/model_detail/{model_id}/
+    #/wganbrwoser/model_delete/
+    path('model_delete/<int:model_id>/',views.model_delete,name='model_delete'),
+    #/wganbrowser/dataset_detail/{dataset_id}/
     path('dataset_detail/<int:dataset_id>/',views.dataset_detail,name='dataset_detail'),
-    #/wganbrowser/model_detail/{model_id}/
+    #/wganbrowser/modelrun_detail/{modelrun_id}/
     path('modelrun_detail/<int:modelrun_id>/',views.modelrun_detail,name='modelrun_detail'),
-    #/wganbrowser/model_detail/{model_id}/
+    #/wganbrowser/modelsnapshot_detail/{modelsnapshot_id}/
     path('modelsnapshot_detail/<int:modelsnapshot_id>/',views.modelsnapshot_detail,name='modelsnapshot_detail'),
-    #/wganbrowser/model_detail/{model_id}/
+    #/wganbrowser/library_detail/{library_id}/
     path('library_detail/<int:library_id>/',views.library_detail,name='library_detail'),
 ]
--- wganmgr/wganbrowser/views.py
+++ wganmgr/wganbrowser/views.py
@@ -10,6 +10,11 @@ from .forms import *
 def index(request):
     return HttpResponse("Hello, world. You're at the wgan browser index.")
 
+def models(request):
+    all_models=model.objects.all()
+    context={'models':all_models}
+    return render(request,'wganbrowser/model/models.html',context)
+
 def model_detail(request,model_id):
     selected_model = model.objects.get(pk=model_id)
     runs = modelRun.objects.filter(model=model_id)
@@ -20,7 +25,6 @@ def model_detail(request,model_id):
         latest_snapshots=modelSnapshot.objects.filter(modelRun=run.id).order_by('-checkpoint')
         if(latest_snapshots.count()>0):
             latest_snapshot=latest_snapshots[0]
-
         runs_with_snapshot[run]=latest_snapshot
 
     context={
@@ -28,6 +32,7 @@ def model_detail(request,model_id):
         'model':selected_model,
         'runs_with_snapshot':runs_with_snapshot,
     }
+    
     return render(request,'wganbrowser/model/model_detail.html',context)
 
 def model_create(request):
@@ -44,10 +49,15 @@ def model_save(request):
                             library=data['library'],
                             dataset=data['dataset'])
             new_model.save()
-            return model_detail(request,new_model.id)
+            return models(request)
     else:
         return model_create(request)
 
+def model_delete(request,model_id):
+    selected_model = model.objects.get(pk=model_id)
+    if selected_model:
+        selected_model.delete()
+    return models(request)
 
 def dataset_detail(request,dataset_id):
     return HttpResponse("Looking at details for dataset</br> %s" % str(get_object_or_404(dataset,pk=dataset_id)))
