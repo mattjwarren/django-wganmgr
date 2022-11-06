caf93148ddc4bebe2d59a4f053ac75e88d43206f
matt <matthew_j_warren@hotmail.com>
2022-11-06 20:52:27 +0000

    logins and static files setup

--- wganmgr/wganbrowser/views.py
+++ wganmgr/wganbrowser/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 from django.http import HttpResponse
@@ -8,13 +9,15 @@ from .models import *
 from .forms import *
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the wgan browser index.")
+    return models(request)
 
+@login_required
 def models(request):
     all_models=model.objects.all()
     context={'models':all_models}
     return render(request,'wganbrowser/model/models.html',context)
 
+@login_required
 def model_detail(request,model_id):
     selected_model = model.objects.get(pk=model_id)
     runs = modelRun.objects.filter(model=selected_model)
@@ -35,11 +38,13 @@ def model_detail(request,model_id):
 
     return render(request,'wganbrowser/model/model_detail.html',context)
 
+@login_required
 def model_create(request):
     form = modelForm()
     context = {'form':form} 
     return render(request,'wganbrowser/model/model_create.html',context)
 
+@login_required
 def model_save(request):
     if request.method=='POST':
         form=modelForm(request.POST)
@@ -53,20 +58,24 @@ def model_save(request):
     else:
         return model_create(request)
 
+@login_required
 def model_delete(request,model_id):
     selected_model = model.objects.get(pk=model_id)
     if selected_model:
         selected_model.delete()
     return models(request)
 
+@login_required
 def dataset_detail(request,dataset_id):
     return HttpResponse("Looking at details for dataset</br> %s" % str(get_object_or_404(dataset,pk=dataset_id)))
 
+@login_required
 def modelruns(request):
     all_modelruns=modelRun.objects.all()
     context={'modelruns':all_modelruns}
     return render(request,'wganbrowser/modelrun/modelruns.html',context)
 
+@login_required
 def modelrun_detail(request,modelrun_id):
     modelrun = modelRun.objects.get(pk=modelrun_id)
     modelsnapshots = modelSnapshot.objects.filter(modelRun=modelrun)
@@ -79,11 +88,13 @@ def modelrun_detail(request,modelrun_id):
 
     return render(request,'wganbrowser/modelrun/modelrun_detail.html',context)
 
+@login_required
 def modelrun_create(request):
     form = modelRunForm()
     context = {'form':form} 
     return render(request,'wganbrowser/modelrun/modelrun_create.html',context)
 
+@login_required
 def modelrun_save(request):
     if request.method=='POST':
         form=modelRunForm(request.POST)
@@ -121,15 +132,18 @@ def modelrun_save(request):
     else:
         return modelrun_create(request)
 
+@login_required
 def modelrun_delete(request,modelrun_id):
     modelrun = modelRun.objects.get(pk=modelrun_id)
     if modelrun:
         modelrun.delete()
     return modelruns(request)
 
+@login_required
 def modelsnapshot_detail(request,modelsnapshot_id):
     return HttpResponse("Looking at details for modelsnapshot</br> %s" % str(get_object_or_404(modelSnapshot,pk=modelsnapshot_id)))
 
+@login_required
 def library_detail(request,library_id):
     return HttpResponse("Looking at details for library</br> %s" % str(get_object_or_404(library,pk=library_id)))
 
--- wganmgr/wganmgr/settings.py
+++ wganmgr/wganmgr/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'wganmgr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,8 +122,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/wganbrowser/models'
+
+# email needs setting up, in the meantime emails go to console
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
