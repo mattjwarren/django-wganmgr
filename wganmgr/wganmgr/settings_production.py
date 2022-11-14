4caf20ceb769060781a390edef675b7317612110
matt <matthew_j_warren@hotmail.com>
2022-11-14 19:00:22 +0000

    settings tweaks

--- wganmgr/wganmgr/settings_production.py
+++ wganmgr/wganmgr/settings_production.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2grl6bv%e6ngdh8at2w$fr$n3#s1vd6w2s&jlrs(!shd=x31+l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["192.168.1.199"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
