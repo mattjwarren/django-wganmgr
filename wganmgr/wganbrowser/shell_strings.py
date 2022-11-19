27369612012ce16fc79fb4e989ef24ed0cfb56a8
matt <matthew_j_warren@hotmail.com>
2022-11-19 00:09:52 +0000

    \#9 path handling framework and applied to libraries

--- wganmgr/wganbrowser/shell_strings.py
+++ wganmgr/wganbrowser/shell_strings.py
@@ -1,2 +1,5 @@
 SHELL_GET_NEWEST_CKPT_FILE="ls -ltr --full-time %s | grep ckpt | grep -v _temp | tail -1"
 SHELL_TOUCH_HALT="touch %shalt"
+SHELL_DOES_PATH_EXIST='[[ -d %s ]] && echo True || echo False'
+SHELL_MKDIR_P="mkdir -p %s && echo True || echo False"
+SHELL_MV="mv %s %s && echo True || echo False"
--- wganmgr/wganbrowser/views/library.py
+++ wganmgr/wganbrowser/views/library.py
@@ -10,11 +10,122 @@ from django.template import loader
 from wganbrowser.models import *
 from wganbrowser.forms import *
 from wganbrowser.strings import *
+from wganbrowser.shell_strings import *
+
 
 from time import sleep
-
+import re
+import os
 from .package_global import *
 
+
+
 @login_required
-def library_detail(request,library_id):
-    return HttpResponse("Looking at details for library</br> %s" % str(get_object_or_404(library,pk=library_id)))
+def libraries(request):
+    libraries=library.objects.all()
+    libraries=group_records_by_field(libraries,['node_affinity'])
+    context={'libraries':libraries}
+    return render(request,'wganbrowser/library/libraries.html',context)
+
+@login_required
+def detail(request,library_id):
+    lib=library.objects.get(pk=library_id)
+    context={'library':lib}
+    return render(request,"wganbrowser/library/detail.html",context)
+
+
+#@Login_required
+def edit(request,library_id):
+    lib=library.objects.get(pk=library_id)
+    old_full_path=os.path.join(settings.NODE_STORAGE_ROOT,lib.path)
+
+    context={'library':lib,
+             'action':'edit'}
+
+    if request.method=='POST':
+        form=libraryCreateEditForm(request.POST,instance=lib)
+        if form.is_valid():
+            data=form.cleaned_data
+            if not data['node_affinity'] in settings.JENKINS_TRAINING_NODES:
+                context.update({'form':form,
+                         'message': LIBRARY_STORAGE_NODE_UNKNOWN % data['node_affinity'],'library':lib})
+                return render(request,'wganbrowser/library/create_edit.html',context)
+            
+            if bad_chars_in_path(data['path']):
+                context.update({'form':form,
+                         'message': LIBRARY_BAD_CHARS_IN_PATH,'library':lib})
+                return render(request,'wganbrowser/library/create_edit.html',context)                
+
+            full_path=os.path.join(settings.NODE_STORAGE_ROOT,data['path'])
+
+            if does_path_exist_on_node(data['node_affinity'],full_path):
+                context.update({'form':form,
+                         'message': LIBRARY_PATH_ALREADY_USED % data['path'],'library':lib})
+                return render(request,'wganbrowser/library/create_edit.html',context)
+
+            if not move_path(data['node_affinity'],old_full_path,full_path):
+                context.update({'form':form,
+                         'message': LIBRARY_PATH_COULD_NOT_MOVE % (data['node_affinity'],old_full_path,full_path),'library':lib})
+                return render(request,'wganbrowser/library/create_edit.html',context)
+            
+            lib.name=data['name']
+            lib.path=data['path']
+            lib.node_affinity=data['node_affinity']
+
+            lib.save()
+            return libraries(request)
+    else:
+        form=libraryCreateEditForm(instance=lib)
+    context.update({'form':form})
+    return render(request,'wganbrowser/library/create_edit.html',context)
+
+@login_required
+def delete(request,library_id):
+    lib=library.objects.get(pk=library_id)
+    lib.delete()
+    return libraries(request)
+
+@login_required
+def create(request):
+    context={'action':'create'}
+
+    if request.method == 'POST':
+        form=libraryCreateEditForm(request.POST)
+        if form.is_valid():
+            data=form.cleaned_data
+
+
+            context={'action':'create'}
+            if not data['node_affinity'] in settings.JENKINS_TRAINING_NODES:
+                context.update({'form':form,
+                         'message': LIBRARY_STORAGE_NODE_UNKNOWN % data['node_affinity']})
+                return render(request,'wganbrowser/library/create_edit.html',context)
+            
+            if bad_chars_in_path(data['path']):
+                context.update({'form':form,
+                         'message': LIBRARY_BAD_CHARS_IN_PATH})
+                return render(request,'wganbrowser/library/create_edit.html',context)                
+
+            full_path=os.path.join(settings.NODE_STORAGE_ROOT,data['path'])
+
+            if does_path_exist_on_node(data['node_affinity'],full_path):
+                context.update({'form':form,
+                         'message': LIBRARY_PATH_ALREADY_USED % data['path']})
+                return render(request,'wganbrowser/library/create_edit.html',context)
+
+            #try and create
+            if not create_path_on_node(data['node_affinity'],full_path):
+                context.update({'form':form,
+                         'message': LIBRARY_PATH_COULD_NOT_CREATE % data['path']})
+                return render(request,'wganbrowser/library/create_edit.html',context)
+
+            new_library=library(name=data['name'],
+                                path=data['path'],
+                                node_affinity=data['node_affinity'])
+            new_library.save()
+            return libraries(request)
+    else:
+        form=libraryCreateEditForm()
+    context.update({'form':form})
+    return render(request,'wganbrowser/library/create_edit.html',context)
+
